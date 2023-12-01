f62a0cfca016d430f4d6f4d8900d3a639340f59a
hjpatel2104 <hjpatel2104@gmail.com>
2023-12-01 15:51:48 +0530

    some changes

--- db_save.py
+++ db_save.py
@@ -2,12 +2,12 @@ from database import Mysql
 import pandas as pd
 
 def main() -> None:
-    data_list = pd.read_parquet('dataFiles/Silence of Nature5.parquet').to_dict('records')
+    data_list = pd.read_parquet('dataFiles/Wild Kingdom Wonders4.parquet').to_dict('records')
     for data_dict in data_list:
         database_obj = Mysql()
         try:
-            data_dict = database_obj.select(query_str="select cat_id as category_id from category where cat_name = %s", query_params=(data_dict['category']), is_fetch_one=True)
-            category_id, img_file = data_dict['category_id'], 'images/' + data_dict['imgSrc'].split('/')[-1]
+            cat_dict = database_obj.select(query_str="select cat_id as category_id from category where cat_name = %s", query_params=(data_dict['category']), is_fetch_one=True)
+            category_id, img_file = cat_dict['category_id'], 'images/' + data_dict['imgSrc'].split('/')[-1]
             print(data_dict['imgSrc'], img_file, category_id)
             database_obj.insert_update_delete(query_str="insert into images(img_src, img_file, category_id) values(%s, %s, %s)", query_params=(data_dict['imgSrc'], img_file, category_id))
         except Exception as e:
--- download_img.py
+++ download_img.py
@@ -23,7 +23,7 @@ def download_image(url, index):
     else:
         print(f"{index + 1}. Failed to download the image. HTTP status code:", response.status_code)
 
-df = pd.read_parquet('dataFiles/Minimal Birds Wildlife5.parquet')
+df = pd.read_parquet('dataFiles/Wild Kingdom Wonders4.parquet')
 url_list = df['imgSrc'].to_list()
 
 for index, url in enumerate(url_list, 0):  # Start the index at 1
