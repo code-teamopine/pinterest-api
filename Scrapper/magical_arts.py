8a48e5250045788c89a26f170b06bd8be86f79d1
hjpatel2104 <hjpatel2104@gmail.com>
2024-01-06 15:28:06 +0530

    add couple images

--- Scrapper/magical_arts.py
+++ Scrapper/magical_arts.py
@@ -11,5 +11,35 @@ driver.get("https://www.magicals.art/search/label/Cute%20Friends")
 time.sleep(6)
 
 def scrolller() -> None:
-    # all_data_a_list = 
-    pass
+    try:
+        all_data_a_list = driver.find_elements(By.XPATH, """//a[@class="thmb"]""")
+        for a_tag in all_data_a_list:
+            aSoup = BeautifulSoup(a_tag.get_attribute('innerHTML'), "html.parser")
+            img_tag = aSoup.find('img')
+            if img_tag:
+                imgSrcList.append({"category": 'winter', "imgSrc": img_tag['data-src'].replace('w1620', 'w1440-h2560')})
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        driver.get(driver.find_element(By.XPATH, """//a[@class="olLnk"]""").get_attribute('href'))
+        time.sleep(6)
+    except Exception as e:
+        print("\n\n", e, "\n\n")
+
+scrolller()
+scrolller()
+scrolller()
+scrolller()
+scrolller()
+scrolller()
+scrolller()
+
+driver.get("https://www.magicals.art/search/label/Night")
+time.sleep(6)
+scrolller()
+scrolller()
+scrolller()
+
+driver.get("https://www.magicals.art/search/label/Old%20Couples")
+time.sleep(6)
+scrolller()
+pd.DataFrame(imgSrcList).to_parquet('dataFiles/couple.parquet', index=False)
+driver.quit()
--- download_img.py
+++ download_img.py
@@ -6,7 +6,7 @@ from random import randint
 import pandas as pd
 
 new_data_list = []
-category = 'lake'
+category = 'couple'
 
 def download_image(url, index):
     response = requests.get(url)
