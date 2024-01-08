4646691ec9df6418f8c130afa936b31de77e2175
hjpatel2104 <hjpatel2104@gmail.com>
2024-01-08 12:50:45 +0530

    image & category Added

--- download_img.py
+++ download_img.py
@@ -6,7 +6,7 @@ from random import randint
 import pandas as pd
 
 new_data_list = []
-category = 'motorcycles'
+category = 'superheroes'
 
 def download_image(url, index):
     response = requests.get(url)
